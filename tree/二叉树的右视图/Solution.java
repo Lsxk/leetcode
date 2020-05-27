3b3abae5d45272dcc925ae16ec8073389d0924df
w00454330 <wangjuzhou@huawei.com>
2020-05-27 20:32:24 +0800

    commite 二叉树的右视图

---
+++ tree/二叉树的右视图/Solution.java
@@ -0,0 +1,27 @@
+package 二叉树的右视图;
+
+import java.util.List;
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * 功能描述：
+ * 二叉树的层序遍历
+ * @version 1.0.0
+ * @since 2020-05-27
+ */
+public class Solution {
+
+
+    public List<Integer> rightSideView(TreeNode root) {
+        LinkedBlockingQueue
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+}
